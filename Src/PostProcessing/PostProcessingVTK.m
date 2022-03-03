4fc90c9b11a145aabbf810c28c2f3beb285d1f8e
Adrià <avillacrosa@gmail.com>
2022-03-03 16:32:18 +0100

    QoL - VTK cell connectivity

--- Src/PostProcessing/PostProcessingVTK.m
+++ Src/PostProcessing/PostProcessingVTK.m
@@ -3,4 +3,5 @@ function PostProcessingVTK(Geo, Set, Step)
     CreateVtkCellAll(Geo, Set, Step)
 	CreateVtkFaceCentres(Geo, Set, Step)
     CreateVtkTet(Geo, Set, Step); 
+    CreateVtkConn(Geo, Set, Step); 
 end
